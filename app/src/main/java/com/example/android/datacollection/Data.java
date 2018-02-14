919744099ed4f35b18a7612569864f9c431a0f80
Khash <khash.021@gmail.com>
2018-02-14 02:05:36 -0800

    Add getDataList method to Data Class (Make all Data objects to a message)

--- app/src/main/java/com/example/android/datacollection/Data.java
+++ app/src/main/java/com/example/android/datacollection/Data.java
@@ -4,6 +4,8 @@ package com.example.android.datacollection;
  * Created by Khashayar on 2/13/2018.
  */
 
+import java.util.ArrayList;
+
 /**
  * This class is responsible for out Data object which will include all of the information
  * we collect
@@ -21,4 +23,21 @@ public class Data {
     Data(){}
 
 
+    //Create a message with all the current Data objects to be sent out
+    public static String getDataList (ArrayList<Data> dataArray) {
+        String list = "";
+        Data data = new Data();
+        int g, c, p;
+        for (int i=0; i<dataArray.size(); i++ ) {
+            data = dataArray.get(i);
+            //sets the value of g (garbage to 1 if the boolean value is true, and 0 if false
+            g = (data.garbage) ? 1:0;
+            c = (data.container) ? 1:0;
+            p = (data.paper) ? 1:0;
+            list += "\n" + Integer.toString(i) + " ; " + data.lat + " : " + data.lon + " ; " +
+                    Integer.toString(g) + " : " + Integer.toString(c) + " : " +
+                    Integer.toString(p) + " .";
+        }
+        return list;
+    }
 }

e27624b8511866577b2148d18b5b67f02ef7f3bd
Khash <khash.021@gmail.com>
2018-02-13 23:36:28 -0800

    Add Data class to manage data collection

---
+++ app/src/main/java/com/example/android/datacollection/Data.java
@@ -0,0 +1,24 @@
+package com.example.android.datacollection;
+
+/**
+ * Created by Khashayar on 2/13/2018.
+ */
+
+/**
+ * This class is responsible for out Data object which will include all of the information
+ * we collect
+ */
+
+public class Data {
+
+    double lat = 0.0;
+    double lon = 0.0;
+    boolean garbage = false;
+    boolean container = false;
+    boolean paper = false;
+
+    //default constructor
+    Data(){}
+
+
+}

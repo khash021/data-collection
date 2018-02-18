24329ae3fb640cc536f50123ee25a068187a5c88
Khash <khash.021@gmail.com>
2018-02-18 15:56:54 -0800

    Add Data classes

---
+++ app/src/main/java/com/example/android/datacollection/Database/DataContract.java
@@ -0,0 +1,89 @@
+package com.example.android.datacollection.Database;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Khashayar on 2/17/2018.
+ *  * This is the class that contains the schema of the table and all the constants for out DB
+ */
+
+public class DataContract {
+
+    //Default empty constructor
+    private DataContract(){}
+
+    // Inner class that defines the table contents
+    public static class DataEntry implements BaseColumns {
+
+        /*
+                Note that all of these Strings are the object data type of those names;
+                and not the actual data type of the database in the corresponding column.
+         */
+
+        public static final String TABLE_NAME = "Location Data";
+        /**
+         * Unique ID number for the pet (only for use in the database table).
+         *
+         * Type: INTEGER
+         */
+        public final static String _ID = BaseColumns._ID;
+        /**
+         * Latitude of the datapoint
+         *
+         * Type: REAL
+         */
+        public final static String COLUMN_LOCATION_LATITUDE ="Latitude";
+
+        /**
+         * Longitude of datapoint
+         *
+         * Type: REAL
+         */
+        public final static String COLUMN_LOCATION_LONGITUDE = "Longitude";
+
+        /**
+         * Garbage
+         *
+         * The only possible values are {@link #GARBAGE_TRUE}, {@link #GARBAGE_FALSE},
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_LOCATION_GARBAGE = "Garbage";
+        /**
+         * Container
+         *
+         * The only possible values are {@link #CONTAINER_TRUE}, {@link #CONTAINER_FALSE},
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_LOCATION_CONTAINER = "Container";
+        /**
+         * Paper
+         *
+         * The only possible values are {@link #PAPER_TRUE}, {@link #PAPER_FALSE},
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_LOCATION_PAPER = "Paper";
+
+        /**
+         * Comments
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_LOCATION_COMMENT = "Comments";
+
+        /**
+         * Possible values for the garbage, container, paper.
+         */
+        public static final int GARBAGE_TRUE = 1;
+        public static final int GARBAGE_FALSE = 0;
+        public static final int CONTAINER_TRUE = 1;
+        public static final int CONTAINER_FALSE = 0;
+        public static final int PAPER_TRUE = 1;
+        public static final int PAPER_FALSE = 0;
+
+
+    }//DataEntry
+
+}//main
---
+++ app/src/main/java/com/example/android/datacollection/Database/DataDbHelper.java
@@ -0,0 +1,64 @@
+package com.example.android.datacollection.Database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.android.datacollection.Database.DataContract.DataEntry;
+
+/**
+ * Created by Khashayar on 2/17/2018.
+ * This is the class which creates the database
+ */
+
+public class DataDbHelper extends SQLiteOpenHelper {
+
+    public static final String LOG_TAG = DataDbHelper.class.getSimpleName();
+
+    /** Name of the database file */
+    private static final String DATABASE_NAME = "location.db";
+    /**
+     * Database version. If you change the database schema, you must increment the database version.
+     */
+    private static final int DATABASE_VERSION = 1;
+
+    /**
+     * Constructs a new instance of {@link DataDbHelper}.
+     *
+     * @param context of the app
+     */
+    public DataDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }//DataDbHelper
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Create a String that contains the SQL statement to create the pets table
+        /*
+        Remember the SQLite command:
+        CREATE TABLE <table name> (<column_name> <column_datatype>, .....)
+        We are doing the same thing except we make it a String constant so we can just pass in the
+        String to the method (execSQL) instead of writing it everytime
+         */
+        String SQL_CREATE_DATA_TABLE = "CREATE TABLE " + DataEntry.TABLE_NAME + " (" +
+                DataEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                DataEntry.COLUMN_LOCATION_LATITUDE + "REAL NOT NULL," +
+                DataEntry.COLUMN_LOCATION_LONGITUDE + "REAL NOT NULL," +
+                DataEntry.COLUMN_LOCATION_GARBAGE + " INTEGER NOT NULL DEFAULT 0," +
+                DataEntry.COLUMN_LOCATION_CONTAINER + " INTEGER NOT NULL DEFAULT 0," +
+                DataEntry.COLUMN_LOCATION_PAPER + " INTEGER NOT NULL DEFAULT 0," +
+                DataEntry.COLUMN_LOCATION_COMMENT + "TEXT);";
+
+        // Execute the SQL statement
+        //Note; execSQL is not a static method and we run that on the SQLiteDatabase object (db)
+        //that was passed in as an input argument in the onCreate method
+        db.execSQL(SQL_CREATE_DATA_TABLE);
+
+    }//onCreate
+
+    @Override
+    public void onUpgrade(SQLiteDatabase DB, int oldVersion, int newVersion) {
+        // The database is still at version 1, so there's nothing to do be done here.
+
+    }//onUpgrade
+}//main
